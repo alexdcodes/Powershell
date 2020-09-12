6297c7d470fc0f6d15d3e5129613d2d3138de668
Alex Diker <alex.diker@live.ca>
2020-09-12 16:29:35 -0400

    Update whoam.ps1

--- whoam.ps1
+++ whoam.ps1
@@ -1,15 +1,15 @@
 #whoami.ps1 | where {$_.type -eq "Group"} | Select Groupname | Sort GroupName
 
-whomi /groups/ fo list | Select -Skip 4 | Where \{$_\} |\
-foreach-object -Begin \{$i=0; $hash=@\{\}\} -Process \{\
-	if (i$ -ge 4) \{\
-	[PSCustomObject]$hash\
-	$hash.Clear()\
-	$i=0\
-\}\
+whomi /groups/ fo list | Select -Skip 4 | Where \{$_} |
+foreach-object -Begin \{$i=0; $hash=@\{\}\} -Process {
+	if (i$ -ge 4) {
+	[PSCustomObject]$hash
+	$hash.Clear()
+	$i=0
+}
 	else\
-		\{\
-		$data=$_-split \'93:\'94\
-		$hash.Add($data[0].Replace(\'93 \'93, \'93\'94),$data[1].Trim())\
-	\}\
-\} }
+		{
+		$data=$_-split ":"
+		$hash.Add($data[0].Replace(" ", ""),$data[1].Trim())
+	}
+} }
